1ef019aff6e7e2c205a21c9780a8edec632ab1ac
Marcin Staszak <Marcin Staszak>
2018-04-06 16:39:04 +0200

    First release: app structure

---
+++ UserInterface/Core/Race.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public enum Race{ Pilzner, Hefe_Weizen, Porter, IPA, RIS, Bock, Barley_Wine, Ale, Witbier, Grodziskie };   
+}
---
+++ UserInterface/Core/Speciality.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public enum Speciality { Smoked, Sour, Barrel_Aged, Whisky, Double, Imperial, Scotish, Doppel, White, Dark, Dunkel };
+}
+
